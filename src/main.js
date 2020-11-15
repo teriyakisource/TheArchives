aada285a929a1ae0f7687464e00a7c7c569646f4
teriyakisource <teriyakivass@gmail.com>
2020-11-15 11:50:52 +0000

    Version 1.0

--- src/main.js
+++ src/main.js
@@ -3,22 +3,72 @@ function signOut(){
     console.log("signing out")
     firebase.auth().signOut().then(function() {
         
-        window.location.href = "index.html"
+        window.location.href = "../index.html"
     }).catch(function(error) {
         alert(error.message)
     }); 
 }
 
 
-function getContent()
+
+function getContent(loc)
 {
-    firebase.database().ref("TheArchives").once('value', function(snapshot)
+    firebase.database().ref("TheArchives/"+loc).once('value', function(snapshot)
     {
+        var items = 0;
         snapshot.forEach(function(childSnapshot)
         {
-            alert(snapshot.val());
+            items++;
+            
         })
+
+        for (i=1;i<items+1;i++)
+        {
+            
+            firebase.database().ref("TheArchives/" + loc + "/" + i.toString()).once('value', function(snapshot)
+            {
+                var p = []
+                
+                snapshot.forEach(function(childSnapshot)
+                {
+                    
+                    
+                    p.push(childSnapshot.val())
+                   
+
+                })
+                console.log(p)
+
+                document.getElementById("content").innerHTML += "<a href='content.html#" + p[2] +"'><div class='thumb'> <div class='thumbcontent'>   <h3>" + p[1] + "<img src='" + p[0] + "'</div></div></a>"
+                
+                
+                
+            })
+        }
+
     })
 }
 
-getContent()
+
+
+
+
+// getContent("videos")
+
+
+
+
+
+var w = 500;
+var h = 500;
+$('#thumbcontent').find('img').each(function(n, image){
+    var image = $(image);
+    image.attr({src : image.attr('src').replace(/s\B\d{2,4}/,'s' + w + '-h' + h +'-c')});
+    image.attr('width',w);
+    image.attr('height',h);
+});
+
+
+
+
+
